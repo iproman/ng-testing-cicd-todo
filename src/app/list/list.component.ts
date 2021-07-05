20817d52266dcc2404b02fc4df8c3828a16011af
iproman <itechman@yandex.ru>
2021-07-05 11:58:53 +0500

    Remove ChangeDetectionStrategy

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,11 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Todo } from '../Todo';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
-  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListComponent {
   @Input() todos: Todo[] = [];

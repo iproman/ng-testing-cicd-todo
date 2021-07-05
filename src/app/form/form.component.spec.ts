69700ecf13dd95f6259bab191ffdbaa3fc7ea106
iproman <itechman@yandex.ru>
2021-07-05 11:56:00 +0500

    Add form test

--- src/app/form/form.component.spec.ts
+++ src/app/form/form.component.spec.ts
@@ -1,25 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FormComponent } from './form.component';
+import { first } from 'rxjs/operators';
+import { Todo } from '../Todo';
 
 describe('FormComponent', () => {
   let component: FormComponent;
-  let fixture: ComponentFixture<FormComponent>;
+  // let fixture: ComponentFixture<FormComponent>;
 
-  beforeEach(async () => {
+  /*beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ FormComponent ]
     })
     .compileComponents();
-  });
+  });*/
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new FormComponent();
+    // fixture = TestBed.createComponent(FormComponent);
+    // component = fixture.componentInstance;
+    // fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create FormComponent', done => {
     expect(component).toBeTruthy();
+    done();
+  });
+
+  it('#addTodo should emit value', done => {
+    const t: Todo = {
+      id: 123,
+      name: 'Todo name'
+    }
+    component.sendTodo
+      .pipe(first())
+      .subscribe(todo => {
+        expect(todo.name).toEqual(t.name);
+      })
+    component.addTodo('Todo name');
+    done();
+  });
+
+  it('#getNumber should return number', done => {
+    const testString = '-a12-b3';
+    const resNum = 123;
+
+    expect(component.getNumber(testString))
+      .toEqual(resNum);
+
+    done();
   });
 });

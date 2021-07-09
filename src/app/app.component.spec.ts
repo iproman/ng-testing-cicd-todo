967d358a3f8473177ec82355fde804de5f8cf8cb
iproman <itechman@yandex.ru>
2021-07-09 17:03:48 +0500

    Add tests to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,15 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<any>;
+  let component: AppComponent;
+  let fixtureData: {
+    todo: any,
+    todos: any
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -14,9 +21,46 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixtureData = {
+      todo: {
+        id: 4,
+        name: 'Antonio',
+      },
+      todos: [
+        {
+          id: 1,
+          name: 'Mark',
+        },
+        {
+          id: 2,
+          name: 'Jacob',
+        },
+        {
+          id: 3,
+          name: 'Larry the Bird',
+        },
+      ]
+    }
+  })
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('check todos', done => {
+    expect(component.todos).toBeFalsy();
+    component.ngOnInit();
+    expect(component.todos).toEqual(fixtureData.todos);
+    done();
+  });
+
+  it('should add todo to list of todos', done => {
+    component.ngOnInit();
+    component.addTodo(fixtureData.todo);
+    expect(component.todos).toContain(fixtureData.todo);
+    done();
   });
 });
